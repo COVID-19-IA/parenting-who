8e5fce65178275a6e29535056116bde71e177e1e
masuP9 <masuP9@gmail.com>
2020-03-31 01:19:59 +0900

    add content info

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
+const dayjs = require("dayjs");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   eleventyConfig.addWatchTarget("./src/style/");
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateFormat", (v) => {
+    return dayjs(v).format("YYYY年MM月DD日 HH:mm");
+  });
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateISOFormat", (v) => {
+    return dayjs(v).format("YYYY-MM-DDTHH:mmZ[Z]");
+  });
 
   return {
     dir: { input: "src", output: "docs" },
